03a680d875e902fe20f61bb611bcd46436756a62
Kennon Stewart <kennon.stewart@gmail.com>
2025-04-13 14:57:54 +0000

    updating sample data

--- migration-analysis/notebooks/migrationDashboard.js
+++ migration-analysis/notebooks/migrationDashboard.js
@@ -1,16 +1,26 @@
-import { Inputs, Plot } from "@observablehq/inputs";
+import { plot, line, geo } from "@observablehq/plot";
+import * as d3 from "d3";
+import { feature } from "topojson-client";
+import us from "@observablehq/us-atlas/us-10m.json" assert { type: "json" };
 
-export const chart = () => {
-  return Plot.plot({
+const states = feature(us, us.objects.states);
+
+export function chart() {
+  return plot({
+    projection: "albers-usa",
+    width: 800,
+    height: 500,
     marks: [
-      Plot.barY(
-        [
-          { country: "Mexico", migrants: 120 },
-          { country: "India", migrants: 90 },
-          { country: "China", migrants: 75 }
-        ],
-        { x: "country", y: "migrants" }
-      )
+      geo(states, { fill: "#f3f3f3", stroke: "#aaa" }),
+      line(migrationData, {
+        x1: d => d.origin_lon,
+        y1: d => d.origin_lat,
+        x2: d => d.dest_lon,
+        y2: d => d.dest_lat,
+        stroke: "steelblue",
+        strokeOpacity: 0.6,
+        strokeWidth: d => Math.sqrt(d.migrants) / 10
+      })
     ]
   });
-};
+}
